ab4bd756a3377e91972057607d7c1ec1929f5ce8
Nikites <rockstar.gg@mail.ru>
2022-09-16 22:09:08 +0300

    add interface: IWeapon, ISingleShootGun, IAutoShootGun, IReloadWeapon, Repair, IThrowWeapon. Add class HandGun, Knife

--- WarcraftInterface/Program.cs
+++ WarcraftInterface/Program.cs
@@ -1,2 +1,126 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace WarcraftInterface;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        var makarov = new HandGun(11) { Damage = 23};
+        makarov.Fire();
+        makarov.AutoShoot();
+        makarov.Reload();
+
+        var makarovSingle = (IWeapon)makarov;
+        //makarovSingle.Fire();
+
+        var makarovAuto = (IAutoShootGun)makarov;
+        // makarovAuto.AutoShoot();
+
+
+        var butterfly = new Knife();
+        butterfly.Throw();
+        butterfly.Fire();
+
+
+    }
+
+    interface IWeapon
+    {
+        public int Damage { get; set; }
+
+        public void Fire();
+    }
+
+    interface ISingleShootGun : IReloadWeapon
+    {
+        public void SingleShoot();
+    }
+
+    interface IThrowWeapon
+    {
+        void Throw();
+    }
+
+    interface IAutoShootGun : IReloadWeapon
+    {
+        public void AutoShoot();
+    }
+
+    interface IReloadWeapon
+    {
+       public int CurrentMagazineSize { get; set; }
+       public int MaxMagazineSize { get; set; }
+       public void Reload();
+    }
+
+    interface Repair
+    {
+        public int State { get; set; }
+        public void Repair();
+    }
+
+
+
+
+    class Knife : IWeapon, IThrowWeapon
+    {
+        public int Damage { get; set; }
+        public int State { get; set; }
+
+        public Knife()
+        {
+
+        }
+
+        public void Fire()
+        {
+            Console.WriteLine("Тыщ!");
+        }
+
+        public void Throw()
+        {
+            Console.WriteLine("Хтыщ!");
+        }
+    }
+
+    class HandGun: IWeapon, IAutoShootGun
+    {
+        public int Damage { get; set;}
+        public int CurrentMagazineSize { get; set;}
+        public int MaxMagazineSize { get; set;}
+        public int State{ get; set;}
+
+        public HandGun(int magazineSize)
+        {
+            MaxMagazineSize = magazineSize;
+            CurrentMagazineSize = magazineSize;
+        }
+
+        public void Repair()
+        {
+            State += 1;
+            Console.WriteLine("пистолет отремонтирован!");
+        }
+
+        public void Upgrade()
+        {
+            Damage += 10;
+            Console.WriteLine("пистолет улучшен!");
+        }
+
+        public void Fire()
+        {
+            Console.WriteLine("ту!");
+        }
+
+        public void AutoShoot()
+        {
+            Console.WriteLine("ту! ту! ту! ту!");
+        }
+
+        public void Reload()
+        {
+            Console.WriteLine("пистолет перезаряжен!");
+        }
+    }
+
+}
